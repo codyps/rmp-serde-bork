25683e241438ac80af253af64a86375112c722f5
Cody P Schafer <dev@codyps.com>
2015-09-03 16:09:16 -0400

    init

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+#![cfg_attr(feature = "serde_macros", feature(custom_derive, plugin))]
+#![cfg_attr(feature = "serde_macros", plugin(serde_macros))]
+
+extern crate serde;
+
+#[cfg(feature = "serde_macros")]
+include!("main.rs.in");
+
+#[cfg(not(feature = "serde_macros"))]
+include!(concat!(env!("OUT_DIR"), "/main.rs"));
